f01ab7ca572a2a4857c5f5ac14f2defbcb7bdfc0
Ivan Firestone <ivan.firestone@icloud.com>
2023-04-06 00:42:57 -0400

    ChatGPT Clone

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import "./index.css";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [value, setValue] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState([]);
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
+      const data = await response.json();
+      console.log(data);
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(previousChats);
+  useEffect(() => {
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats((previousChats) => [
+        ...previousChats,
+        {
+          title: currentTitle,
+          role: "user",
+          content: value,
+        },
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content,
+        },
+      ]);
+    }
+  }, [message, currentTitle, value]);
+
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>Primer Chat</li>
+        </ul>
+        <nav>
+          <p>Made By Dexter</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>DexterGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>
+              ➢
+            </div>
+          </div>
+          <p className="info">
+            Chat GPT Mar 14 Version. Free Research Preview. Our goal is to make
+            AI systems more natural and safe to interact with. Your feedback
+            will help us improve.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default App;
